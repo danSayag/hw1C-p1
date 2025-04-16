26713d97931b161f3c75800eba4220269bcbae7f
Dan <dansayag02062001@gmail.com>
2025-04-16 19:41:49 +0300

    second Commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            menuStrip1 = new MenuStrip();
+            newGameToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(24, 24);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { newGameToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(615, 33);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            newGameToolStripMenuItem.Size = new Size(114, 29);
+            newGameToolStripMenuItem.Text = "New Game";
+            newGameToolStripMenuItem.Click += newGameToolStripMenuItem_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(615, 352);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Margin = new Padding(2);
             Name = "Form1";
-            Text = "Form1";
+            Text = "fifteen puzzle";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem newGameToolStripMenuItem;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -3,7 +3,7 @@ namespace WinFormsApp1
     public partial class Form1 : Form
     {
 
-       
+
         Button[,] buttons = new Button[4, 4];
         public Form1()
         {
@@ -11,7 +11,7 @@ namespace WinFormsApp1
             createBoard();
         }
 
-        
+
 
 
 
@@ -40,41 +40,42 @@ namespace WinFormsApp1
         private void createBoard()
         {
             Random rnd = new Random();
-            newGameButton();
 
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 4; j++)
                 {
-                   
+
                     buttons[i, j] = new Button();
                     buttons[i, j].Size = new Size(80, 80);
                     buttons[i, j].Location = new Point(j * buttons[i, j].Width, i * buttons[i, j].Height + 50);
 
                     Color c = Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(2, 255));
                     while (colorExist(c))
-                        c = Color.FromArgb(rnd.Next(0,256), rnd.Next(0,256), rnd.Next(0,256));
+                        c = Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
                     buttons[i, j].BackColor = c;
                     int num = rnd.Next(1, 16);
                     if (i == 3 && j == 3)
                     {
                         num = 1000;
                     }
-             
-                    while (numExist(num))
-                        num = rnd.Next(1,16);
 
+                    while (numExist(num))
+                        num = rnd.Next(1, 16);
+
+
+                    buttons[i, j].Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular);
 
                     buttons[i, j].TabIndex = i * 4 + j;
                     buttons[i, j].Text = num.ToString();
                     buttons[i, j].Click += btnClick;
                     buttons[i, j].Visible = true;
-                    if( i == 3 &&  j == 3 )
-                        buttons[i, j].Visible = false ;
+                    if (i == 3 && j == 3)
+                        buttons[i, j].Visible = false;
                     this.Controls.Add(buttons[i, j]);
-                    
+
                 }
-               
+
 
 
 
@@ -83,11 +84,35 @@ namespace WinFormsApp1
         }
 
 
+
+
+
+        //הבדיקה לסיום משחק באמיתית
+        //private bool checkWin()
+        //{
+        //    int count = 1;
+        //    for (int i = 0; i < 4; i++)
+        //    {
+        //        for (int j = 0; j < 4; j++)
+        //        {
+        //            if (i == 3 && j == 3)
+        //                return true;
+
+        //            if (buttons[i, j] == null || buttons[i, j].Text != count.ToString())
+        //                return false;
+
+        //            count++;
+        //        }
+        //    }
+        //    return true;
+        //}
+
+
         private void newGameButton()
         {
             Button button = new Button();
             button.Text = "New Game";
-            button.Size = new Size(80*4, 50);
+            button.Size = new Size(80 * 4, 50);
             button.Location = new Point(0, 0);
             button.Click += newGameClick;
             this.Controls.Add(button);
@@ -100,20 +125,20 @@ namespace WinFormsApp1
             int i = btn.TabIndex / 4;
             int j = btn.TabIndex % 4;
 
-            if (j < 3 && buttons[i, j+1].Visible == false)
+            if (j < 3 && buttons[i, j + 1].Visible == false)
             {
-               
-                buttons[i , j + 1].Visible = true;
-                buttons[i , j + 1].Text = buttons[i, j].Text;
+
+                buttons[i, j + 1].Visible = true;
+                buttons[i, j + 1].Text = buttons[i, j].Text;
                 buttons[i, j + 1].BackColor = buttons[i, j].BackColor;
                 buttons[i, j].Visible = false;
-              
+
             }
-            if(i < 3 && buttons[i + 1, j].Visible == false)
+            if (i < 3 && buttons[i + 1, j].Visible == false)
             {
-                buttons[i+1, j].Visible = true;
-                buttons[i+1, j].Text = buttons[i, j].Text;
-                buttons[i+1, j].BackColor = buttons[i, j].BackColor;
+                buttons[i + 1, j].Visible = true;
+                buttons[i + 1, j].Text = buttons[i, j].Text;
+                buttons[i + 1, j].BackColor = buttons[i, j].BackColor;
                 buttons[i, j].Visible = false;
             }
             if (i > 0 && buttons[i - 1, j].Visible == false)
@@ -125,9 +150,9 @@ namespace WinFormsApp1
             }
             if (j > 0 && buttons[i, j - 1].Visible == false)
             {
-                buttons[i , j - 1].Visible = true;
-                buttons[i , j - 1].Text = buttons[i, j].Text;
-                buttons[i , j - 1].BackColor = buttons[i, j].BackColor;
+                buttons[i, j - 1].Visible = true;
+                buttons[i, j - 1].Text = buttons[i, j].Text;
+                buttons[i, j - 1].BackColor = buttons[i, j].BackColor;
                 buttons[i, j].Visible = false;
             }
             if (winGame())
@@ -141,33 +166,33 @@ namespace WinFormsApp1
         }
 
 
-        
 
-       
 
-        
+
+
+
 
 
 
         public void createWinBox()
         {
-            DialogResult result = MessageBox.Show("New Game?","Game over!",MessageBoxButtons.YesNo);
+            DialogResult result = MessageBox.Show("New Game?", "Game over!", MessageBoxButtons.YesNo);
 
             if (result == DialogResult.Yes)
             {
                 clearBoard();
-                createBoard(); 
+                createBoard();
             }
             else
             {
-                Environment.Exit(0); 
+                Environment.Exit(0);
             }
         }
 
 
 
 
-        private bool winGame()  
+        private bool winGame()
         {
             if (buttons[0, 0] != null && buttons[0, 0].Text == "1" &&
                 buttons[0, 1] != null && buttons[0, 1].Text == "2")
@@ -179,9 +204,19 @@ namespace WinFormsApp1
 
 
         private void clearBoard()
-        { 
-            this.Controls.Clear();
-            buttons = new Button[4, 4];
+        {
+            for (int i = 0; i < 4; i++)
+            { 
+                for(int j = 0; j < 4; j++) 
+                {
+                    if(buttons[i, j] != null)
+                    {
+                        this.Controls.Remove(buttons[i, j]);
+                    }
+                }
+            }
+            buttons = new Button[4,4];
+
 
         }
 
@@ -200,8 +235,10 @@ namespace WinFormsApp1
 
         }
 
-
-
-
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clearBoard();
+            createBoard();
+        }
     }
 }
